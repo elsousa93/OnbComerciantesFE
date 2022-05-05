8e9d7111e991e4c0986bbb2d838384106db4be26
elsousa93 <99330081+elsousa93@users.noreply.github.com>
2022-05-05 18:12:35 +0100

    Added comments to the tariffs component

--- src/app/commercial-offer/commercial-offer-tariff/commercial-offer-tariff.component.ts
+++ src/app/commercial-offer/commercial-offer-tariff/commercial-offer-tariff.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IPricing } from '../IPricing.interface';
 import { ITariff } from '../ITariff.interface';
 
 @Component({
@@ -11,6 +10,7 @@ import { ITariff } from '../ITariff.interface';
 })
 export class CommercialOfferTariffComponent implements OnInit {
 
+  /*Tariffs structure*/
   tariffs: ITariff[] = [];
   public clientID: number = 12345678;
 
@@ -18,9 +18,11 @@ export class CommercialOfferTariffComponent implements OnInit {
   selectedNonTarif: string = "";
 
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string, private route: Router) {
+    /*Get Tariffs List*/
     http.get<ITariff[]>(baseUrl + 'becommercialoffer/GetTariff/' + this.clientID).subscribe(result => {
       this.tariffs = result;
       console.log(this.tariffs)
+      /*Get the first element to display in each table*/
       this.selectedTarif = this.tariffs.findIndex(x => x.package === 'Tarifa').toString();
       this.selectedNonTarif = this.tariffs.findIndex(x => x.package != 'Tarifa').toString();
 
@@ -32,10 +34,7 @@ export class CommercialOfferTariffComponent implements OnInit {
     console.log(this.selectedTarif)
   }
 
-  onChangeCheckbox($event: any) {
-    console.log(this.tariffs)
-  }
-
+  /*Controls the Continue Button*/
   onCickContinue() {
     console.log(this.selectedTarif)
     this.tariffs[this.selectedTarif].selected = true;
