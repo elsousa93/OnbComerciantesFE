2f4a2405338fdcebc83ae98b950e4bfca5a5171d
Rita Silva Capela <ricapela@neyond.pt>
2022-05-18 15:11:24 +0100

    Pep finished. Chnages in Stakes e Client - FE

--- src/app/pep/IPep.interface.ts
+++ src/app/pep/IPep.interface.ts
@@ -1,36 +1,31 @@
 
     export interface IPep    {
-      id: number;
-      clientNr: number;
-
+      id: number,
       pep12months: boolean;        //É, ou foi nos últimos 12 meses, Pessoa Politicamente Exposta (PEP)?:
-        pepType: string;
-        pepCountry: string;
-        pepSinceWhen: string;
+      pepType: string;
+      pepCountry: string;
+      pepSinceWhen: string;
 
       pepFamiliarOf: boolean;        //é familiar de uma PEP
-        pepFamilyRelation: string;
+      pepFamilyRelation: string;
 
       pepRelations: boolean;       // Mantem relações de natureza societária ou comercial com uma PEP
-        pepTypeOfRelation: string;
+      pepTypeOfRelation: string;
 
       pepPoliticalPublicJobs: boolean; // É, ou foi nos últimos 12 meses, titular de outros Cargos Políticos ou Públicos em território nacional?
-      pepPoliticalPublicJobDesignation: boolean;
+      pepPoliticalPublicJobDesignation: string;
 
-      relatedPep_type: string; //Identificação do PEP relacionado
+      relatedPep_type: string;
       relatedPep_country: string;
-      relatedPep_sinceWhen: string; //beginning of relationship
+      relatedPep_sinceWhen: string;
       relatedPep_nif: string;
       relatedPep_name: string;
       relatedPep_idNumber: string;
       relatedPep_idDocumentType: string;
       relatedPep_idDocumentValidity: string;
       relatedPep_idDocumentCountry: string;
-    
       relatedPep_address: string;
       relatedPep_addressLocation: string;
       relatedPep_postalCode: string;
       relatedPep_addressCountry: string;
-      
-
   }
