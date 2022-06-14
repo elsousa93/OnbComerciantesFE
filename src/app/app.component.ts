bd92dd7b3507df703338aee98ce7e994483f711d
Diogo Antonio Felix <dfelix@epimetheus.pt>
2022-06-14 18:30:45 +0100

    mudanças sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { MediaMatcher } from '@angular/cdk/layout';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { CookieService } from 'ngx-cookie-service';
@@ -9,14 +10,22 @@ import { CookieService } from 'ngx-cookie-service';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+
   pageName: string = 'Teste';
   title = 'app';
 
   isToggle: boolean = true;
 
-  constructor(public translate: TranslateService, private http: HttpClient, private cookie: CookieService, private router: Router) {
+  constructor(public translate: TranslateService, private http: HttpClient, private cookie: CookieService, private router: Router,
+    changeDetectorRef: ChangeDetectorRef, media: MediaMatcher  ) {
     translate.addLangs(['pt', 'en']);
     translate.setDefaultLang('pt');
+    this.mobileQuery = media.matchMedia('(max-width: 850px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
   ngOnInit() {
@@ -43,5 +52,9 @@ export class AppComponent {
 
   toggleSideNav(toggled: boolean) {
     this.isToggle = toggled;
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 }
