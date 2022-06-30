f1ced708c826194dc3af5057ed63a59033cff336
Joao Carlos Bandeira <jbandeira@epimetheus.pt>
2022-06-30 16:35:05 +0100

    commit da correção do /BEConfig

--- src/app/table-info/table-info.service.ts
+++ src/app/table-info/table-info.service.ts
@@ -10,38 +10,38 @@ export class TableInfoService {
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
 
   GetAllCountries(): any {
-    return this.http.get<CountryInformation[]>(this.baseUrl + 'BEConfig/GetAllCountries');
+    return this.http.get<CountryInformation[]>(this.baseUrl + 'GetAllCountries');
   }
 
   GetCountryById(code: string): any {
-    return this.http.get<CountryInformation>(this.baseUrl + 'BEConfig/GetCountry/' + code);
+    return this.http.get<CountryInformation>(this.baseUrl + 'GetCountry/' + code);
   }
 
   GetAllCAEs() {
-    return this.http.get<EconomicActivityInformation[]>(this.baseUrl + 'BEConfig/GetAllCAEs');
+    return this.http.get<EconomicActivityInformation[]>(this.baseUrl + 'GetAllCAEs');
   }
 
   GetAllLegalNatures() {
-    return this.http.get<LegalNature[]>(this.baseUrl + 'BEConfig/GetAllLegalNatures');
+    return this.http.get<LegalNature[]>(this.baseUrl + 'GetAllLegalNatures');
   }
 
   GetAllStakeholderRoles() {
-    return this.http.get<StakeholderRole[]>(this.baseUrl + 'BEConfig/GetAllStakeholderRoles');
+    return this.http.get<StakeholderRole[]>(this.baseUrl + 'GetAllStakeholderRoles');
   }
 
   GetAllShopActivities() {
-    return this.http.get<ShopActivity[]>(this.baseUrl + 'BEConfig/GetAllShopActivities');
+    return this.http.get<ShopActivity[]>(this.baseUrl + 'GetAllShopActivities');
   }
 
   GetAllPEPTypes() {
-    return this.http.get<PEPTypes>(this.baseUrl + 'BEConfig/GetAllPEPTypes');
+    return this.http.get<PEPTypes>(this.baseUrl + 'GetAllPEPTypes');
   }
 
   GetAllPOS() {
-    return this.http.get<POS[]>(this.baseUrl + 'BEConfig/GetAllPOS');
+    return this.http.get<POS[]>(this.baseUrl + 'GetAllPOS');
   }
 
   GetAllProducts() {
-    return this.http.get<Product[]>(this.baseUrl + 'BEConfig/GetAllProducts');
+    return this.http.get<Product[]>(this.baseUrl + 'GetAllProducts');
   }
 }
