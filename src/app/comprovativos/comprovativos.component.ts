e481803c316f258a6c573e4410f1af7366a194c7
Filipe Magalhaes <filipe.magalhaes@decode.pt>
2022-07-11 18:56:25 +0100

    update code comprovativos

--- src/app/comprovativos/comprovativos.component.ts
+++ src/app/comprovativos/comprovativos.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Renderer2 } from '@angular/core';
 import { Component, Inject, Input, OnInit, VERSION, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Subscription, take } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
+import { Client } from '../client/Client.interface';
 import { DataService } from '../nav-menu-interna/data.service';
 import { CheckDocumentsComponent } from './check-documents/check-documents.component';
 import { IComprovativos } from './IComprovativos.interface';
@@ -17,6 +18,113 @@ import { ComprovativosService } from './services/comprovativos.services';
 })
 export class ComprovativosComponent implements OnInit {
   public comprovativos: IComprovativos[] = [];
+
+  public newComp: IComprovativos = {
+    "id": 0,
+    "clientId": "",
+    "filename": "",
+    "url": ""
+  };
+
+  public client: Client = {
+    "clientId": "",
+    "fiscalId": "",
+    "companyName": "",
+    "contactName": "",
+    "shortName": "",
+    "observations": "",
+    "headquartersAddress": {
+      "address": "",
+      "postalCode": "",
+      "postalArea": "",
+      "locality": "",
+      "country": ""
+    },
+    "merchantType": "",
+    "legalNature": "",
+    "legalNature2": "",
+    "crc": {
+      "code": "",
+      "validUntil": ""
+    },
+    "shareCapital": {
+      "capital": 0,
+      "date": "1966-08-30"
+    },
+    "bylaws": "",
+    "mainEconomicActivity": {
+      "code": "",
+      "branch": ""
+    },
+    "otherEconomicActivities": [
+      {
+        "code": "",
+        "branch": ""
+      },
+      {
+        "code": "",
+        "branch": ""
+      }
+    ],
+    "mainOfficeAddress": {
+      "address": "",
+      "postalCode": "",
+      "postalArea": "",
+      "country": ""
+    },
+    "establishmentDate": "2009-12-16",
+    "businessGroup": {
+      "type": "",
+      "fiscalId": ""
+    },
+    "sales": {
+      "estimatedAnualRevenue": 0,
+      "averageTransactions": 0,
+      "servicesOrProductsSold": [
+        "",
+        ""
+      ],
+      "servicesOrProductsDestinations": [
+        "",
+        ""
+      ]
+    },
+    "foreignFiscalInformation": {
+      "issuerCountry": "",
+      "issuanceIndicator": "",
+      "fiscalId": "",
+      "issuanceReason": ""
+    },
+    "bankInformation": {
+      "bank": "",
+      "branch": "",
+      "iban": "",
+      "accountOpenedAt": "2019-06-11"
+    },
+    "contacts": {
+      "preferredMethod": "",
+      "preferredPeriod": {
+        "startsAt": "22:40:00.450Z",
+        "endsAt": "15:42:54.722Z"
+      },
+      "phone1": {
+        "countryCode": "",
+        "phoneNumber": ""
+      },
+      "phone2": {
+        "countryCode": "",
+        "phoneNumber": ""
+      },
+      "fax": {
+        "countryCode": "",
+        "phoneNumber": ""
+      },
+      "email": ""
+    },
+    "documentationDeliveryMethod": "",
+    "billingEmail": ""
+  };
+
   public result: any;
   public id: number = 0;
   public clientNr: number = 0;
@@ -25,6 +133,8 @@ export class ComprovativosComponent implements OnInit {
   public currentPage: number;
   public map: Map<number, boolean>;
 
+  public url: string;
+
   pageName = "";
   file?: File;
   localUrl: any;
@@ -32,30 +142,40 @@ export class ComprovativosComponent implements OnInit {
   urlImage: string | any;
   mostrar: boolean = false;
   deleteModalRef: BsModalRef | undefined;
+  checkListModalRef: BsModalRef | undefined;
+  compClientId;
 
-  clienteEmpresa: string;
+  comerciante: string;
   comprovantes: string;
   intervenientes: string;
   lojas: string;
   oferta: string;
   info: string;
 
+  filename: any;
+  clientId: any;
 
 
   @ViewChild('deleteModal') deleteModal;
+  @ViewChild('checkListDocs') checkListDocs;
   constructor(public http: HttpClient, @Inject('BASE_URL') baseUrl: string, private route: Router, private router: ActivatedRoute, private compService: ComprovativosService, private renderer: Renderer2,
-    private modalService: BsModalService, private data: DataService) {
+  private modalService: BsModalService, private data: DataService) {
 
+    this.url = baseUrl;
     this.ngOnInit();
-    http.get<IComprovativos[]>(baseUrl + `BEComprovativos/`).subscribe(result => {
+    http.get<Client>(baseUrl + 'BEClients/GetClientById/' + this.clientNr).subscribe(result => {
+      this.client = result;
+    }, error => console.error(error));
+    http.get<IComprovativos[]>(baseUrl + `BEComprovativos/` + this.clientNr).subscribe(result => {
       this.comprovativos = result;
+      console.log(result);
     }, error => console.error(error));
     this.updateData(true, 2);
   }
 
   ngOnInit(): void {
     this.clientNr = Number(this.router.snapshot.params['id']);
-    console.log(String(this.router.snapshot.params['pagename']));
+    //console.log(String(this.router.snapshot.params['pagename']));
     this.pageName = String(this.router.snapshot.params['pageName']);
     this.subscription = this.data.currentData.subscribe(map => this.map = map);
     this.subscription = this.data.currentPage.subscribe(currentPage => this.currentPage = currentPage);
@@ -73,23 +193,32 @@ export class ComprovativosComponent implements OnInit {
     const sizeFile = this.file.size / (1024 * 1024);
     var extensoesPermitidas = /(.pdf)$/i;
     const limSize = 10;
-    if ((sizeFile <= limSize) && (extensoesPermitidas.exec(this.file.name))) {
-      if (event.target.files && event.target.files[0]) {
-        var reader = new FileReader();
-        reader.onload = (event: any) => {
-          this.localUrl = event.target.result;
+
+    this.newComp.clientId = String(this.clientNr);
+    console.log(this.clientNr);
+    this.http.post(this.url + 'ServicesComprovativos/GetLastId/', this.newComp).subscribe(result => {
+      console.log(result);
+      if (result != null) {
+        this.compClientId = result;
+        if ((sizeFile <= limSize) && (extensoesPermitidas.exec(this.file.name))) {
+          if (event.target.files && event.target.files[0]) {
+            var reader = new FileReader();
+            reader.onload = (event: any) => {
+              this.localUrl = event.target.result;
+            }
+            reader.readAsDataURL(event.target.files[0]);
+            this.uploadFile(comp.clientId, this.compClientId);
+          }
+        } else {
+          alert("Verifique o tipo / tamanho do ficheiro");
         }
-        reader.readAsDataURL(event.target.files[0]);
-        this.uploadFile(comp[0].id);
       }
-    } else {
-      alert("Verifique o tipo / tamanho do ficheiro");
-    }
+    }, error => console.error(error));
   }
 
-  uploadFile(id: any) {
+  uploadFile(id: any, compClient: any) {
     if (this.file != undefined) {
-      this.compService.uploadFile(this.file, id).subscribe(data => {
+      this.compService.uploadFile(this.file, id, compClient).subscribe(data => {
         if (data != null) {
           alert("Upload efetuado!");
           var elemento = document.getElementById(id);
@@ -130,99 +259,41 @@ export class ComprovativosComponent implements OnInit {
     link.click();
   }
 
-  onDelete(id: any) {
+  onDelete(id: any, clientId: any, filename: any) {
+    this.id = id;
+    this.clientId = clientId;
+    this.filename = filename;
     this.deleteModalRef = this.modalService.show(this.deleteModal, { class: 'modal-sm' });
-  }
-
-  confirmDelete(id: any) {
-    this.compService.delFile(id)
-    //this.compService.deleteFile(id);
-    this.deleteModalRef?.hide();
-  }
-
-  declineDelete() {
-    this.deleteModalRef?.hide();
-  }
-
-  openModalWithComponent(ident: any) {
-    this.modalService.show(CheckDocumentsComponent);
   }
 
   obterStakeholders() {
     this.route.navigate(['stakeholders/']);
   }
 
-  onClick(e) {
-    console.log(e);
-    switch (e) {
-      case null:
-        this.clienteEmpresa = 'fas fa-circle icone-menu-secundario';
-        this.comprovantes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.intervenientes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.lojas = 'far fa-circle icone-menu-secundario-inactivo';
-        this.oferta = 'far fa-circle icone-menu-secundario-inactivo';
-        this.info = 'far fa-circle icone-menu-secundario-inactivo';
-        break;
-
-      case "CLIENTE EMPRESA":
-        this.clienteEmpresa = 'fas fa-circle icone-menu-secundario';
-        this.comprovantes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.intervenientes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.lojas = 'far fa-circle icone-menu-secundario-inactivo';
-        this.oferta = 'far fa-circle icone-menu-secundario-inactivo';
-        this.info = 'far fa-circle icone-menu-secundario-inactivo';
-        break;
-
-      case "COMPROVATIVOS":
-        this.clienteEmpresa = 'far fa-circle icone-menu-secundario-inactivo';
-        this.comprovantes = 'fas fa-circle icone-menu-secundario';
-        this.intervenientes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.lojas = 'far fa-circle icone-menu-secundario-inactivo';
-        this.oferta = 'far fa-circle icone-menu-secundario-inactivo';
-        this.info = 'far fa-circle icone-menu-secundario-inactivo';
-        break;
-
-      case "INTERVENIENTES":
-        this.clienteEmpresa = 'far fa-circle icone-menu-secundario-inactivo';
-        this.comprovantes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.intervenientes = 'fas fa-circle icone-menu-secundario';
-        this.lojas = 'far fa-circle icone-menu-secundario-inactivo';
-        this.oferta = 'far fa-circle icone-menu-secundario-inactivo';
-        this.info = 'far fa-circle icone-menu-secundario-inactivo';
-        break;
-
-      case "LOJAS":
-        this.clienteEmpresa = 'far fa-circle icone-menu-secundario-inactivo';
-        this.comprovantes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.intervenientes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.lojas = 'fas fa-circle icone-menu-secundario';
-        this.oferta = 'far fa-circle icone-menu-secundario-inactivo';
-        this.info = 'far fa-circle icone-menu-secundario-inactivo';
-        break;
-
-      case "OFERTA COMERCIAL":
-        this.clienteEmpresa = 'far fa-circle icone-menu-secundario-inactivo';
-        this.comprovantes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.intervenientes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.lojas = 'far fa-circle icone-menu-secundario-inactivo';
-        this.oferta = 'fas fa-circle icone-menu-secundario';
-        this.info = 'far fa-circle icone-menu-secundario-inactivo';
-        break;
-
-      case "INFO DECLARATIVA":
-        this.clienteEmpresa = 'far fa-circle icone-menu-secundario-inactivo';
-        this.comprovantes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.intervenientes = 'far fa-circle icone-menu-secundario-inactivo';
-        this.lojas = 'far fa-circle icone-menu-secundario-inactivo';
-        this.oferta = 'far fa-circle icone-menu-secundario-inactivo';
-        this.info = 'fas fa-circle icone-menu-secundario';
-        break;
-
-    }
-
+  confirmDelete() {
+    this.deleteModalRef?.hide();
+    this.compService.delFile(this.id,this.clientId,this.filename).subscribe(data => {
+      if (data != null) {
+        alert("Ficheiro apagado com sucesso!!");
+        this.load();
+      }
+    },
+      err => {
+        console.error(err);
+      }
+    )
+    
   }
 
+  onCheckList() {
+    this.checkListModalRef = this.modalService.show(this.checkListDocs, { class: 'modal-xl' });
+  }
+
+  declineDelete() {
+    this.deleteModalRef?.hide();
+  }
+
+  declineCheckList() {
+    this.checkListModalRef?.hide();
+  }
 }
-
-
-
