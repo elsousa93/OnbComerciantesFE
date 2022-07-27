f30bff654b333e96d756bda3518f3758b541c1ef
Joao Carlos Bandeira <jbandeira@epimetheus.pt>
2022-07-27 16:29:20 +0100

    correção de remoção dos countries

--- src/app/aceitacao/aceitacao.component.ts
+++ src/app/aceitacao/aceitacao.component.ts
@@ -4,6 +4,7 @@ import { Component, Inject, Input, OnInit, VERSION, ViewChild } from '@angular/c
 import { FormGroup } from '@angular/forms';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Subscription, take } from 'rxjs';
+import { ProcessGet, ProcessService, UpdateProcess } from '../process/process.service';
 
 
 @Component({
@@ -18,17 +19,23 @@ export class AceitacaoComponent implements OnInit{
   public currentPage: number;
   public subscription: Subscription;
 
+  public processId: string;
+  public process: ProcessGet;
   
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string,
-  private route: Router,
-  private router: ActivatedRoute) {
+    private route: Router,
+    private router: ActivatedRoute, private processService: ProcessService) {
 
     this.ngOnInit();
-  
+    this.processService.getProcessById(this.processId).subscribe(result => {
+      this.process = result;
+    });
+
 }
 
 ngOnInit(): void {
 
+  this.processId = this.router.snapshot.paramMap.get('id');
   var context = this;
 }
 
