a07ac0b1f024b91990a4e45ff161e74b3b0bf694
jandrade391 <jandrade@neyond.pt>
2022-09-05 17:50:43 +0100

    Messageid and error logging

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -10,7 +10,7 @@ export class LoggerService {
   constructor(private logger : NGXLogger) {}
 
   request(method:string, url: string, header: string, body:string, messageId: string ){
-    this.logger.info(`${method} request made to ${url}`, "int", method, url, header, body, messageId);
+    this.logger.info(`${method} request made to ${url}`, "int", method, url, header, body, messageId, "");
   }
 
   responseCustom(url: string, header: string, body:string, messageId: string, status: string  ){
@@ -25,23 +25,23 @@ export class LoggerService {
     this.responseCustom(response.url, JSON.stringify(response.headers), JSON.stringify(response.body), messageId, response.status.toString())
   }
 
-  exception(exception:string, context:string, description: string){
-    this.logger.error(description, "app", exception, context);
+  exception(exception:string, context:string, description: string, messageId: string = ""){
+    this.logger.error(description, "app", exception, context, messageId);
   }
 
-  trace(description: any){
-    this.logger.trace(description, "app");
+  trace(description: any, messageId: string = ""){
+    this.logger.trace(description, "app", "", "", messageId);
   }
 
-  info(description: any){
-    this.logger.info(description, "app");
+  info(description: any, messageId: string = ""){
+    this.logger.info(description, "app", "", "", messageId);
   }
 
-  debug(description: any){
-    this.logger.debug(description, "app");
+  debug(description: any, messageId: string = ""){
+    this.logger.debug(description, "app", "", "", messageId);
   }
 
-  error(description: any, error?: Error){
-    this.logger.error(description, "app", error.name || "", error.stack || "");
+  error(description: any, error?: Error, messageId: string = ""){
+    this.logger.error(description, "app", error.name || "", error.stack || "", messageId);
   }
 }
