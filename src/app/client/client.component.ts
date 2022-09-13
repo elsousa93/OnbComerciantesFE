f0f428879612d0f050a763d25d30ddae9fb0ece7
mrosacor <mcorreia@neyond.pt>
2022-09-13 16:01:46 +0100

    try to fix bugs

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -718,7 +718,6 @@ export class ClientComponent implements OnInit {
     this.errorMsg = '';
     this.resultError = ''
     this.newClientForm = null;
-    this.BlockDocumentNumber = false;
     this.errorInput = 'form-control campo_form_coment';
     if (id == true) {
       this.showENI = false;
