c4cc541e34c0deab5e240fd9ef14c309be55f2b4
mrosacor <mcorreia@neyond.pt>
2022-09-23 16:14:21 +0100

    fix bug on dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -312,6 +312,19 @@ export class DashboardComponent implements OnInit {
         this.returnedCount = result.pagination.total;
       });
     });
+
+        //Pendentes de Aceitação
+        this.processService.searchProcessByState('contractAcceptance', 0, 1).subscribe(result => {
+          this.logger.debug('Pendentes de Aceitação' + result);
+          this.processService.searchProcessByState('contractAcceptance', 0, result.pagination.total).subscribe(resul => {
+            this.contractAcceptanceProcessess = resul;
+            this.dataSourceAceitacao = new MatTableDataSource(this.contractAcceptanceProcessess.items);
+    
+            this.dataSourceAceitacao.paginator = this.paginatorPageAceitacao;
+            this.dataSourceAceitacao.sort = this.empTbSortAceitacao;
+            this.contractAcceptanceCount = result.pagination.total;
+          });
+        });
     
     //Arquivo Fisico
     this.processService.searchProcessByState('Completed', 0, 1).subscribe(result => {
