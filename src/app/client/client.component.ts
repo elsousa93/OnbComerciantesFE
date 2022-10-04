116ac4e838d0c0a8b2c82e6e89b936550a7541f2
mrosacor <margarida.correia78352@gmail.com>
2022-10-04 17:33:02 +0100

    jump to clientbyid

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -644,7 +644,7 @@ export class ClientComponent implements OnInit {
       }
     };
     this.logger.debug("a passar para a proxima pagina");
-    this.route.navigate(['/clientbyid', this.selectedClient.client.fiscalId], navigationExtras);
+    this.route.navigate(['/clientbyid', NIFNIPC], navigationExtras);
 
     //isto nao esta a aparecer na versao mais nova.
   }
