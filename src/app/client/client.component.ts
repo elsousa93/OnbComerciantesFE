6deb8a350d665d820930ba4cc8db63fe6b0e2886
Diogo Antonio Felix <dfelix@epimetheus.pt>
2022-10-20 08:42:09 +0100

    client

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -787,15 +787,6 @@ export class ClientComponent implements OnInit {
     this.selectedClient.idx = clientEmitted.idx;
   }
 
-  //numericOnly(event): boolean { // restrict e,+,-,E characters in  input type number
-  //  const charCode = (event.which) ? event.which : event.keyCode;
-  //  if (charCode == 101 || charCode == 69 || charCode == 45 || charCode == 43) {
-  //    return false;
-  //  }
-  //  return true;
-
-  //}
-
   numericOnly(event): boolean {
     var ASCIICode = (event.which) ? event.which : event.keyCode;
 
