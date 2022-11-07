d688d7c78609b9de197da6b5b9c139876d5b29ef
mrosacor <margarida.correia78352@gmail.com>
2022-11-07 14:28:01 +0000

    try to fix bugs on contactos email

--- src/app/client/info-declarativa/info-declarativa-lojas/info-declarativa-lojas.component.ts
+++ src/app/client/info-declarativa/info-declarativa-lojas/info-declarativa-lojas.component.ts
@@ -46,7 +46,8 @@ export class InfoDeclarativaLojasComponent implements OnInit, AfterViewInit {
 
   listValue!: FormGroup;
   currentIdx: number = 0;
-  phoneNumber: AbstractControl;
+  phone1Number: AbstractControl;
+  phone2Number: AbstractControl;
 
   client: Client;
   returned: string;
@@ -99,8 +100,8 @@ export class InfoDeclarativaLojasComponent implements OnInit, AfterViewInit {
       }, { validators: validPhoneNumber }),
       email: new FormControl(this.selectedStore?.email != null ? this.selectedStore?.email : this.client?.contacts?.email, Validators.email),
     });
-
-    this.phoneNumber = this.listValue.get("phoneNumber");
+    this.phone1Number = this.listValue.get("cellphone").get("phoneNumber");
+    this.phone2Number = this.listValue.get("telephone").get("phoneNumber");
 
     this.listValue.get("cellphone").get("phoneNumber").valueChanges.pipe(distinctUntilChanged()).subscribe(val => {
       if (val != null && val != "") {
